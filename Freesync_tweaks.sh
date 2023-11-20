732a2cb644b9af8f88fab82c441ba29655fd7f7c
gunther6000 <gunther6000@gmail.com>
2023-11-19 20:37:04 -0500

    Create Freesync_tweaks.sh

---
+++ Freesync_tweaks.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# This script attempts to fix shader stutter issues.
+
+# Check if the user is running the script as root
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run this script as root or with sudo."
+  exit 1
+fi
+
+# Disable shader caching
+echo "Disabling shader caching..."
+echo 0 > /proc/sys/vm/nr_hugepages
+
+# Unload and reload the graphics driver (replace 'your_driver_module' with the actual driver module)
+echo "Unloading and reloading the graphics driver..."
+modprobe -r your_driver_module
+modprobe your_driver_module
+
+echo "Shader stutter fix applied successfully."
+
+# Optionally, you may want to reboot your system for changes to take effect
+#echo "Rebooting the system..."
+#reboot
