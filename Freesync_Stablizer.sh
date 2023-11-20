c5374e0ac772a09732124e0a9d60d7f4d0a7e952
gunther6000 <gunther6000@gmail.com>
2023-11-20 09:55:55 -0500

    Create Freesync_Stablizer.sh

---
+++ Freesync_Stablizer.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Set the desired refresh rate (replace 60 with your target refresh rate)
+target_refresh_rate=60
+
+# Get the name of your display (you may need to adjust this)
+display_name=$(xrandr | grep " connected" | awk '{ print$1 }')
+
+# Check if amdgpu-pro is loaded (this may vary based on your system and GPU driver)
+if lsmod | grep amdgpu_pro; then
+  # Set the refresh rate using xrandr
+  xrandr --output $display_name --mode $target_refresh_rate
+else
+  echo "amdgpu-pro not loaded. Make sure you have the correct driver installed."
+fi
